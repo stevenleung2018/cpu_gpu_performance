fb901311d2bb95d61ae1f8f2454f96eae2401078
Steven Leung <klsleung@student.ubc.ca>
2024-06-22 00:05:26 -0700

    Add more prompts for better understanding of what is happening.

--- numpy_cupy_pytorch_comparison.py
+++ numpy_cupy_pytorch_comparison.py
@@ -10,10 +10,12 @@ else:
     cupy_available = False
 # cupy_available = False
 
-size = 20000
+size = 30000
+print(f"Initializing NumPy matrices of {size} x {size}...")
 A_np = np.random.rand(size, size).astype(np.float32)
 B_np = np.random.rand(size, size).astype(np.float32)
 C_np = np.zeros((size, size)).astype(np.float32)
+print("Done initializing.")
 
 def numpy_test(A_np, B_np, C_np):
     start = time.time()
@@ -37,12 +39,17 @@ def cupy_test(A_np, B_np, C_np):
             return None
 
 def pytorch_cpu_test(A_np, B_np, C_np):
+    print(f"Initializing PyTorch matrices of {size}x{size} on CPU...")
     A_pt_cpu = torch.from_numpy(A_np)
     B_pt_cpu = torch.from_numpy(B_np)
+    C_pt_cpu = torch.empty_like(A_pt_cpu)
+    print("Done initializing.")
     start = time.time()
-    C_pt_cpu = torch.mm(A_pt_cpu, B_pt_cpu)
+    torch.mm(A_pt_cpu, B_pt_cpu, out=C_pt_cpu)
     pytorch_cpu_time = time.time() - start
+    """print("Checking matrix dot product from PyTorch (CPU) with that from NumPy...")
     assert np.allclose(C_np, C_pt_cpu.numpy(), rtol=0.0001), "The results from NumPy and PyTorch (CPU) are NOT close!"
+    print("Done checking.")"""
     return pytorch_cpu_time
 
 def pytorch_gpu_test(A_np, B_np, C_np):
@@ -57,38 +64,42 @@ def pytorch_gpu_test(A_np, B_np, C_np):
     device = torch.device(device_name)
 
     if not device_name == 'cpu':
+        print(f"Initializing PyTorch matrices of {size}x{size} on GPU...")
         A_pt_gpu = torch.from_numpy(A_np).to(device)
         B_pt_gpu = torch.from_numpy(B_np).to(device)
         C_pt_gpu = torch.empty_like(A_pt_gpu).to(device)
+        print("Done initializing.")
         start = time.time()
-        C_pt_gpu = torch.mm(A_pt_gpu, B_pt_gpu)
+        torch.mm(A_pt_gpu, B_pt_gpu, out=C_pt_gpu)
         pytorch_gpu_time = time.time() - start
+        """print("Checking matrix dot product from PyTorch (GPU) with that from NumPy...")
         C_np_torch = torch.from_numpy(C_np).to(device)
         assert torch.allclose(C_np_torch, C_pt_gpu, rtol = 0.0001), "The results from NumPy and PyTorch (GPU) are NOT close!"
+        print("Done checking...")"""
         return pytorch_gpu_time
     else:
         print("No compatible GPU is available for PyTorch.")
         return None
 
-print(f"NumPy (CPU): Estimated time of execution: {3.3376 * (size / 10000) ** 3:.2f} seconds")
+print(f"NumPy (CPU): Estimated time of execution: {22.349 * (size / 20000) ** 2:.2f} seconds")
 numpy_time = numpy_test(A_np, B_np, C_np)
 print(f"NumPy (CPU) time: {numpy_time:.4f}")
 
 if cupy_available:
-    print(f"CuPy (GPU): estimated time of execution: {1.599 * (size / 10000):.2f} seconds")
+    print(f"CuPy (GPU): estimated time of execution: {1.599 * (size / 10000) ** 2:.2f} seconds")
     cupy_time = cupy_test(A_np, B_np, C_np)
     print(f"CuPy (GPU) time: {cupy_time:.4f}")
 else:
     print("CuPy not available.  No testing.")
     
-print(f"PyTorch (CPU): Estimated time of execution: {numpy_time / 3.3376 * 3.1231:.2f} seconds")
+print(f"PyTorch (CPU): Estimated time of execution: {numpy_time / 22.3490 * 8.7419:.2f} seconds")
 pytorch_cpu_time = pytorch_cpu_test(A_np, B_np, C_np)
 print(f"PyTorch (CPU) time: {pytorch_cpu_time:.4f}")
 
-print(f"PyTorch (GPU): Estimated time of execution: {0.002 * (size / 10000):.4f} seconds")
+print(f"PyTorch (GPU): Estimated time of execution: {0.09032 * (size / 20000) ** 2:.4f} seconds")
 pytorch_gpu_time = pytorch_gpu_test(A_np, B_np, C_np)
 if pytorch_gpu_time is not None:
     print(f"PyTorch (GPU) time: {pytorch_gpu_time}")
     print(f"PyTorch (GPU) is {pytorch_cpu_time / pytorch_gpu_time:.2f} times faster than PyTorch (CPU).")
 else:
-    print(f"PyTorch has not detected any GPU.  No testing done")
+    print("PyTorch has not detected any GPU.  No testing done.")
